459a57776c16bf1ed8503c8e7ec2fcc0b9d15c52
Ting Wang <felix.wang.1026@gmail.com>
2018-02-06 13:52:22 -0500

    change layout for mobiledesing1

--- MobileDesign1/App.js
+++ MobileDesign1/App.js
@@ -1,49 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Post from './components/Post';
+import { Header, ImageContainer, ButtonsWrapper } from './components';
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      post: {}
-    };
-  }
-
-  componentDidMount() {
-    const post = {
-      image: 'https://www.gettyimages.com/gi-resources/images/frontdoor/creative/LonelyPlanetRM/Slide_2.jpg',
-      comments: [
-        {
-          user: 'Some User',
-          comment: 'A comment on the picture'
-        },
-        {
-          user: 'Another User',
-          comment: 'A comment on the picture'
-        },
-        {
-          user: 'And Another User',
-          comment: 'A comment on the picture'
-        }
-      ]
-    };
-    this.setState(() => {
-      return {
-        post
-      };
-    });
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <View>
-          <Text style={styles.header}>Header</Text>
-        </View>
-        <View>
-          <Post post={this.state.post} />
-        </View>
+      <View style={container}>
+        <Header />
+        <ImageContainer />
+        <ButtonsWrapper />
       </View>
     );
   }
@@ -52,14 +17,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start'
-  },
-  header: {
-    textAlign: 'center',
-    paddingTop: 30,
-    paddingBottom: 10,
-    fontSize: 20
+    backgroundColor: '#fff'
   }
 });
+
+const { container } = styles;
